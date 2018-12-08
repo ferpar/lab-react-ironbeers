2e66f2aa1b53febfda26b3b8f28ecd91bb078a09
Fer Perez Ayala <ferp@protonmail.ch>
2018-12-08 18:41:16 +0100

    Basics already put in place

---
+++ ironbeers-fer/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <nav className="nav text-white bg-primary pl-5"><h1>Ironbeers App !</h1></nav>
+
+      </div>
+    );
+  }
+}
+
+export default App;
