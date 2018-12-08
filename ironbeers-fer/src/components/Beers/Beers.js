fc5dd44d16ca0c069e1365859ed1046b3db4422a
Fer Perez Ayala <ferp@protonmail.ch>
2018-12-08 20:18:23 +0100

    Iteration 3 complete

--- ironbeers-fer/src/components/Beers/Beers.js
+++ ironbeers-fer/src/components/Beers/Beers.js
@@ -6,6 +6,7 @@ export default class Beers extends React.Component {
     render(){
         return(
             <div className="beers">
+            <nav className="nav justify-content-center"><Link to='/'><img src="/images/Home.jpg" alt="home"/></Link></nav>
             <h1>Beers Page</h1>
             </div>
         )
